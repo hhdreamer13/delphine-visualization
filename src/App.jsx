ab2596ffc68f3a74c1093e9e28868dea9a72bd1f
HhDreamer <hh.dreamer@gmail.com>
2023-04-28 22:47:26 +0200

    commit redesign mobile vertical message.

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import {
   RouterProvider,
   Outlet,
 } from "react-router-dom";
+import Lottie from "lottie-react";
 import AllRoutes from "./routes/AllRoutes";
-import rotatePhoneIcon from "./assets/rotate.png";
 import Footer from "./components/Footer/Footer";
 import { ModalProvider } from "./components/ModalContext";
+import responsiveSwitch from "./assets/6051-responsive-preview.json";
 
 const router = createBrowserRouter(createRoutesFromElements(AllRoutes()));
 
@@ -23,18 +24,24 @@ function App() {
             </RouterProvider>
           </ModalProvider>
         </div>
-        <div className="flex h-screen flex-col items-center justify-around sm:hidden">
-          <h2 className="my-14 text-2xl uppercase">PoAnimaViz</h2>
-
-          <img
-            src={rotatePhoneIcon}
-            alt="Rotate phone icon"
-            className="animate-rotate-phone h-24 w-24"
-          />
+        <div className="flex h-screen flex-col items-center justify-center gap-8 sm:hidden">
+          <h2 className="mb-14 text-2xl uppercase">PoAnimaViz</h2>
+          <p className="w-60 text-center">
+            Les visualisations du site sont mieux adaptées aux ordinateurs
+            plutôt qu'aux téléphones.
+          </p>
+          <div className="w-20">
+            <Lottie
+              animationData={responsiveSwitch}
+              loop
+              autoplay
+              speed={0.1}
+            />
+          </div>
           <div>
-            <p className="w-60 text-center">Un grand écran, c'est mieux !</p>
             <p className="w-60 text-center">
-              Mais essayez le mode paysage pour une découverte.
+              Mais il est possible que vous puissiez tenter le mode paysage pour
+              quelques aperçus.
             </p>
           </div>
         </div>
