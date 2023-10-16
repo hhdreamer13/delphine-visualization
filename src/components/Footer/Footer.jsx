b8ff73c1cb42eda153868119db4e8392374ad5aa
HhDreamer <hh.dreamer@gmail.com>
2023-10-17 00:53:08 +0200

    commit fix footer issue.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <footer className="group absolute bottom-0 mb-5 w-full p-4 font-titre text-2xl">
-      <a href="https://www.hhdev.pro/" rel="noreferrer" target="_blanc">
-        <div className="flex items-center justify-center gap-4">
+      <div className="flex items-center justify-center gap-4">
+        <a href="https://www.hhdev.pro/" rel="noreferrer" target="_blanc">
           <span className="flex items-center justify-center drop-shadow-2xl">
             Créé avec
             <span className="mx-2">
@@ -15,8 +15,8 @@ const Footer = () => {
             </span>
             par Hooman
           </span>
-        </div>
-      </a>
+        </a>
+      </div>
     </footer>
   );
 };
